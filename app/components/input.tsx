ca6dec3810a54954657b6fd5fd6f534ce6abf336
Melody Universe <melody@computost.com>
2025-02-16 19:55:34 -0800

    touch input border color

--- app/components/input.tsx
+++ app/components/input.tsx
@@ -6,7 +6,7 @@ export const Input = forwardRef<HTMLInputElement, ComponentProps<"input">>(
   ({ className, type, ...props }, ref) => (
     <input
       className={mergeClassNames(
-        "bg-transparentpx-3 flex h-9 rounded-md border px-3 py-1 text-base shadow-sm transition-colors",
+        "bg-transparentpx-3 flex h-9 rounded-md border border-violet-200 px-3 py-1 text-base shadow-sm transition-colors dark:border-violet-800",
         "placeholder:text-violet-300 placeholder:dark:text-violet-700",
         "focus-visible:ring-1 focus-visible:outline-none",
         "md:text-sm",
