336b4ff8452984b0dcc49166176404519bbb4384
Melody Universe <melody@computost.com>
2025-02-16 20:30:17 -0800

    add some styling to card

--- app/components/card.tsx
+++ app/components/card.tsx
@@ -2,7 +2,7 @@ import type { PropsWithChildren, ReactNode } from "react";
 
 export function Card({ children }: PropsWithChildren): ReactNode {
   return (
-    <section className="space-y-4 rounded-3xl border border-violet-400 p-6 dark:border-violet-700">
+    <section className="flex flex-col items-baseline space-y-4 rounded-3xl border border-violet-400 p-6 dark:border-violet-700">
       {children}
     </section>
   );
