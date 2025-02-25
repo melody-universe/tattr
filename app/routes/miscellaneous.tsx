4a37dbce24491cbdaa2cf876b2f0f0c0ec7f6b23
Melody Universe <melody@computost.com>
2025-02-24 19:32:04 -0800

    render / process asset manager

--- app/routes/miscellaneous.tsx
+++ app/routes/miscellaneous.tsx
@@ -1,10 +1,19 @@
 import type { ReactNode } from "react";
 
+import { parseFormData } from "@mjackson/form-data-parser";
+
 import { Card } from "~/components/card";
 import { PageLayout } from "~/components/page-layout";
+import { AssetManager } from "~/modules/asset-manager";
 import { PrinterTester } from "~/modules/printer-tester";
+import { assets } from "~/utils/assets.server";
+import { getSession } from "~/utils/sessions.server";
 
-export default function Miscellaneous(): ReactNode {
+import type { Route } from "./+types/miscellaneous";
+
+export default function Miscellaneous({
+  loaderData,
+}: Route.ComponentProps): ReactNode {
   return (
     <PageLayout>
       <Card>
@@ -12,6 +21,44 @@ export default function Miscellaneous(): ReactNode {
         yet. I&apos;m sure I&apos;ll find a better place for them in the future.
       </Card>
       <PrinterTester />
+      {loaderData.userId && <AssetManager />}
     </PageLayout>
   );
+}
+
+export async function action({ context, request }: Route.ActionArgs) {
+  const session = await getSession(request.headers.get("Cookie"));
+  const userId = session.get("userId");
+  if (!userId) {
+    return { error: "You are not logged in.", isSuccess: false };
+  }
+
+  const formData = await parseFormData(request);
+
+  const name = formData.get("name")?.valueOf();
+  if (typeof name !== "string" || name.trim().length === 0) {
+    return { error: "Expected name", isSuccess: false };
+  }
+
+  const contents = formData.get("contents");
+  if (!(contents instanceof File) || contents.size === 0) {
+    return { error: "Expected contents", isSuccess: false };
+  }
+
+  const response = await assets(context).createAsset({
+    contents: await contents.arrayBuffer(),
+    name,
+    userId,
+  });
+
+  if (!response.isSuccess) {
+    return response;
+  }
+
+  return { assetId: response, isSuccess: true };
+}
+
+export async function loader({ request }: Route.LoaderArgs) {
+  const session = await getSession(request.headers.get("Cookie"));
+  return { userId: session.get("userId") };
 }
